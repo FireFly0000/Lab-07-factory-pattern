3dfbcb9c8013a63dfb1b5169b8a7037560ba5204
joeypercia <jperc004@ucr.edu>
2021-05-17 19:24:43 -0700

    Added unit testing for factory class

---
+++ factory_test.hpp
@@ -0,0 +1,51 @@
+#ifndef __FACTORY_TEST_HPP__
+#define __FACTORY_TEST_HPP__
+
+#include "gtest/gtest.h"
+#include "mult.hpp"
+#include "add.hpp"
+#include "sub.hpp"
+#include "div.hpp"
+#include "pow.hpp"
+#include "op.hpp"
+#include "factory.hpp"
+
+TEST(FactoryTest, AddingValues) { 
+	Factory test;
+	const char *args[] = {"2", "+", "2"};
+	Base *op = test.parse(args, 3);
+	EXPECT_DOUBLE_EQ(op->evaluate(), 4);
+}   
+
+TEST(FactoryTest, AddingAndSubtracting) {
+        Factory test;
+        const char *args[] = {"10", "-", "2", "+", "3","-","4"};
+        Base *op = test.parse(args, 7);
+        EXPECT_DOUBLE_EQ(op->evaluate(), 7);
+}
+
+TEST(FactoryTest, MultandDiv) {
+        Factory test;
+        const char *args[] = {"5", "*", "10","/","25","/","2"};
+        Base *op = test.parse(args, 7);
+        EXPECT_DOUBLE_EQ(op->evaluate(), 1);
+}
+
+TEST(FactoryTest, Pow) {
+        Factory test;
+        const char *args[] = {"3", "*","*", "3"};
+        Base *op = test.parse(args, 4);
+        EXPECT_DOUBLE_EQ(op->evaluate(), 27);
+}
+
+TEST(FactoryTest, AllOperators) {
+        Factory test;
+        const char *args[] = {"10", "+", "5","*","2","/","15","*","*","3","-","4"};
+        Base *op = test.parse(args, 12);
+        EXPECT_DOUBLE_EQ(op->evaluate(), 4);
+}
+
+#endif
+
+
+
