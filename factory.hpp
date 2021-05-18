b42ab430b6fb03dc794be55b6aaf46dd7c93c81b
joeypercia <jperc004@ucr.edu>
2021-05-17 18:33:53 -0700

    Make main.cpp work with command line arguments
    I think it's fully working but could use more testing

--- factory.hpp
+++ factory.hpp
@@ -16,9 +16,9 @@ class Factory
 private:
 
 public:
-	Base* parse(char* input[], int length)
+	Base* parse(const char* input[], int length)
 	{
-		int i = 1;
+		int i = 0;
 		Base* root = nullptr;
 		Base* lop = nullptr;
 		Base* rop = nullptr;
@@ -26,36 +26,46 @@ public:
 
 		while (i < length) {
 			//if (std::isdigit(std::stoi(input[i])) == 0) {
-			if (input[i] == "0" || input[i] == "1" || input[i] == "2" || input[i] == "3" || input[i] == "4" || input[i] == "5" ||
-				input[i] == "6" || input[i] == "7" || input[i] == "8" || input[i] == "9") {
+			if (*input[i] == '0' || *input[i] == '1' || *input[i] == '2' || *input[i] == '3' || *input[i] == '4' || *input[i] == '5' ||
+				*input[i] == '6' || *input[i] == '7' || *input[i] == '8' || *input[i] == '9') {
 				lop = new Op(std::stod(input[i++]));
 			}
+			/*if (std::isdigit(std::stoi(input[i])) == 0) {
+				lop = new Op(std::stod(input[i++]));
+			}*/
 			else {
 				lop = rop;
 			}
-			if (input[i] == "+") {
+			if (*input[i] == '+') {
 				root = new Add(lop, new Op(std::stod(input[++i])));
 				rop = root;
 			}
-			else if (input[i] == "-") {
+			else if (*input[i] == '-') {
 				root = new Sub(lop, new Op(std::stod(input[++i])));
 				rop = root;
 			}
-			else if (input[i] == "/") {
+			else if (*input[i] == '/') {
 				root = new Div(lop, new Op(std::stod(input[++i])));
 				rop = root;
 			}
-			else if (input[i] == "*") {
-				//if (input[i+1] == "*")
-				root = new Mult(lop, new Op(std::stod(input[++i])));
-				rop = root;
+			else if (*input[i] == '*') {
+				if (*input[i + 1] == '*') {
+					++i;
+					root = new Pow(lop, new Op(std::stod(input[++i])));
+					rop = root;
+					
+				}
+				else {
+					root = new Mult(lop, new Op(std::stod(input[++i])));
+					rop = root;
+				}
 			}
-			else if (input[i] == "**") {
+		    /*else if (*input[i] == '**') {
 				root = new Pow(lop, new Op(std::stod(input[++i])));
 				rop = root;
-			}
+				}*/
 			else {
-				std::cout << "ERROR, INVALID INPUTS" << std::endl;
+				std::cout << "ERROR, " << input[i] << " IS NOT A VALID INPUT" << std::endl;
 				return nullptr;
 			}
 			++i;
--- main.cpp
+++ main.cpp
@@ -20,11 +20,15 @@ int main(int argc, char* argv[]) {
     // all the classes that you create (and can be instantiated) in this lab
 
     Factory test;
-   //const char* args[] = { "4", "**", "3", "-", "32", "+", "16", "/", "2", "*", "3" };
-    Base* op = test.parse(argv, argc);
+    const char* args[100];
+    for (int i = 0; i < argc - 1; ++i) {
+        args[i] = argv[i + 1];
+    }
 
-    for (int i = 1; i < argc; ++i) {
-        cout << argv[i];
+    Base* op = test.parse(args, argc-1);
+
+    for (int i = 0; i < argc-1; ++i) {
+        cout << args[i];
     }
     cout << " = " << op->evaluate() << endl;
 
