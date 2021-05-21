b34427c821ed28520e79d0f72a86e3f26127fc6c
joeypercia <jperc004@ucr.edu>
2021-05-20 19:14:05 -0700

    Update factory_test.hpp

--- factory_test.hpp
+++ factory_test.hpp
@@ -12,45 +12,60 @@
 
 TEST(FactoryTest, AddingValues) { 
 	Factory test;
-	const char *args[] = {"2", "+", "2"};
+	char *args[] = {"2", "+", "2"};
 	Base *op = test.parse(args, 3);
 	EXPECT_DOUBLE_EQ(op->evaluate(), 4);
 }   
 
 TEST(FactoryTest, AddingAndSubtracting) {
         Factory test;
-        const char *args[] = {"10", "-", "2", "+", "3","-","4"};
+        char *args[] = {"10", "-", "2", "+", "3","-","4"};
         Base *op = test.parse(args, 7);
         EXPECT_DOUBLE_EQ(op->evaluate(), 7);
 }
 
 TEST(FactoryTest, MultandDiv) {
         Factory test;
-        const char *args[] = {"5", "*", "10","/","25","/","2"};
+        char *args[] = {"5", "*", "10","/","25","/","2"};
         Base *op = test.parse(args, 7);
         EXPECT_DOUBLE_EQ(op->evaluate(), 1);
 }
 
 TEST(FactoryTest, Pow) {
         Factory test;
-        const char *args[] = {"3", "*","*", "3"};
+        char *args[] = {"3", "*","*", "3"};
         Base *op = test.parse(args, 4);
         EXPECT_DOUBLE_EQ(op->evaluate(), 27);
 }
 
 TEST(FactoryTest, AllOperators) {
         Factory test;
-        const char *args[] = {"10", "+", "5","*","2","/","15","*","*","3","-","4"};
+        char *args[] = {"10", "+", "5","*","2","/","15","*","*","3","-","4"};
         Base *op = test.parse(args, 12);
         EXPECT_DOUBLE_EQ(op->evaluate(), 4);
 }
 
 TEST(FactoryTest, InvalidInput) {
 	Factory test;
-	const char *args[] = {"10", "x", "4"};
+	char *args[] = {"10", "x", "4"};
         Base *op = test.parse(args, 3);
         EXPECT_TRUE(op == nullptr);
 }
+
+TEST(FactoryTest, DoubleMultandDiv) {
+    Factory test;
+    char* args[] = { "7.8", "*", "-3.2", "/", "2.0" };
+    Base* op = test.parse(args, 5);
+    EXPECT_DOUBLE_EQ(op->eavluate(), -12.48);
+}
+
+TEST(FactoryTest, MultiplyingTwoNeg) {
+    Factory test;
+    char* args[] = { "-9.0", "*", "-2.0"};
+    Base* op = test.parse(args, 3);
+    EXPECT_DOUBLE_EQ(op->eavluate(), 18);
+}
+
 #endif
 
 
