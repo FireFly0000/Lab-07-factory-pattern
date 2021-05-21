93a3e500abadb0386230b85f5294b96bd1dac2d0
joeypercia <81598667+joeypercia@users.noreply.github.com>
2021-05-20 19:26:43 -0700

    Update factory_test.hpp

--- factory_test.hpp
+++ factory_test.hpp
@@ -12,56 +12,56 @@
 
 TEST(FactoryTest, AddingValues) { 
 	Factory test;
-	char *args[] = {"2", "+", "2"};
+	const char *args[] = {"2", "+", "2"};
 	Base *op = test.parse(args, 3);
 	EXPECT_DOUBLE_EQ(op->evaluate(), 4);
 }   
 
 TEST(FactoryTest, AddingAndSubtracting) {
         Factory test;
-        char *args[] = {"10", "-", "2", "+", "3","-","4"};
+        const char *args[] = {"10", "-", "2", "+", "3","-","4"};
         Base *op = test.parse(args, 7);
         EXPECT_DOUBLE_EQ(op->evaluate(), 7);
 }
 
 TEST(FactoryTest, MultandDiv) {
         Factory test;
-        char *args[] = {"5", "*", "10","/","25","/","2"};
+        const char *args[] = {"5", "*", "10","/","25","/","2"};
         Base *op = test.parse(args, 7);
         EXPECT_DOUBLE_EQ(op->evaluate(), 1);
 }
 
 TEST(FactoryTest, Pow) {
         Factory test;
-        char *args[] = {"3", "*","*", "3"};
+        const char *args[] = {"3", "*","*", "3"};
         Base *op = test.parse(args, 4);
         EXPECT_DOUBLE_EQ(op->evaluate(), 27);
 }
 
 TEST(FactoryTest, AllOperators) {
         Factory test;
-        char *args[] = {"10", "+", "5","*","2","/","15","*","*","3","-","4"};
+        const char *args[] = {"10", "+", "5","*","2","/","15","*","*","3","-","4"};
         Base *op = test.parse(args, 12);
         EXPECT_DOUBLE_EQ(op->evaluate(), 4);
 }
 
 TEST(FactoryTest, InvalidInput) {
 	Factory test;
-	char *args[] = {"10", "x", "4"};
+	const char *args[] = {"10", "x", "4"};
         Base *op = test.parse(args, 3);
         EXPECT_TRUE(op == nullptr);
 }
 
 TEST(FactoryTest, DoubleMultandDiv) {
     Factory test;
-    char* args[] = { "7.8", "*", "-3.2", "/", "2.0" };
+    const char* args[] = { "7.8", "*", "-3.2", "/", "2.0" };
     Base* op = test.parse(args, 5);
     EXPECT_DOUBLE_EQ(op->evaluate(), -12.48);
 }
 
 TEST(FactoryTest, MultiplyingTwoNeg) {
     Factory test;
-    char* args[] = { "-9.0", "*", "-2.0"};
+    const char* args[] = { "-9.0", "*", "-2.0"};
     Base* op = test.parse(args, 3);
     EXPECT_DOUBLE_EQ(op->evaluate(), 18);
 }
