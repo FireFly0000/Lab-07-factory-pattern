0c4fa590f1796430f0262997cc4a4bc2b835599a
FireFly0000 <wktfirefly283@gmail.com>
2021-05-20 22:17:19 -0700

    attempting to add command line pass in

--- main.cpp
+++ main.cpp
@@ -14,30 +14,48 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char**argv) {
     // This is a very basic main, and being able to correctly execute this main
     // does not constitute a completed lab. Make sure you write unit tests for
     // all the classes that you create (and can be instantiated) in this lab
 
     Factory test;
-    const char* args[] = { "-14.2", "**", "3", "-", "-32", "+", "16", "/", "2", "*", "!3"}; 
-    //const char* args[] = { "-4.2", "-", ".309"};
-    Base* op = test.parse(args, 11);                                                       
+    //char* args[] = { "-14.2", "**", "3", "-", "-32", "+", "16", "/", "2", "*", "3"}; 
+    //char* args[] = { "4", "+", "3"};
+    //Base* op = test.parse(args, 11);                                                       
+    
+    //cout << op->stringify();
+    //cout << " = " << op->evaluate() << endl;
+    
 
-    for (int i = 0; i < 11; ++i) {
-        cout << args[i];
+ 
+    char* args[argc-1];
+    for(unsigned i = 1; i < argc-1; i++){
+	args[i] = argv[i];
     }
-    cout << " = " << op->evaluate() << endl;
+    for (int i = 0; i < argc-1; ++i) {
+    	cout << args[i];
+    }		
+    cout << endl;
+    //Base* op = test.parse(args, argc);
 
+    //cout << " = " << op->evaluate();
+    cout << endl;
+   
+//=================================== 
+   /* Factory* Test = new Factory();
+    
+    for(unsigned i = 0; i < argc; i++) {
+      cout << argv[i];
+    }
 
-    /*Base* three = new Op(3);
-    Base* seven = new Op(7.0);
-    Base* four = new Op(4);
-    Base* two = new Op(2);
-    Base* mult = new Mult(seven, four);
-    Base* add = new Add(three, mult);
-    Base* minus = new Sub(add, two);
+    Base* op = Test->parse(argv, argc);
+    if(!op) {
+      cout << "invalid expression Tree" << endl;
+    } else {
+      cout << op->stringify() << " = " << op->evaluate() << endl;
+    }
+    delete Test;*/
 
-    std::cout << minus->stringify() << " = " << minus->evaluate() << std::endl;*/
     return 0;
 }
